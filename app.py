713c0accedece177ca9bf616cab38d4c1cee91a7
Adlet Karimov <adekarimov@gmail.com>
2026-04-09 21:25:07 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+def lcm(a, b):
+    import math
+    return abs(a * b) // math.gcd(a, b)
+
+@app.route("/adekarimov_gmail_com")
+def compute():
+    x = request.args.get("x")
+    y = request.args.get("y")
+
+    try:
+        x = int(x)
+        y = int(y)
+
+        if x < 0 or y < 0:
+            return "NaN"
+
+        return str(lcm(x, y))
+
+    except:
+        return "NaN"
+
+if __name__ == "__main__":
+    app.run()
