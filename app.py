87f29b07a1a846f3fe9f04a149b67edf9c9f3e5c
Adlet Karimov <adekarimov@gmail.com>
2026-04-09 21:41:01 +0500

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,10 @@ def compute():
     except:
         return "NaN"
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
 # redeploy
